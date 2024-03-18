618dd0a1450669e3f128e72cf7e726f3c8107711
Uladzislau Blok <blokv75@gmail.com>
2024-03-18 20:00:25 +0100

    feat(ride-dev): Create mail class and application.yaml

---
+++ ride/src/main/java/org/blokdev/RideMicroserviceApplication.java
@@ -0,0 +1,11 @@
+package org.blokdev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideMicroserviceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RideMicroserviceApplication.class, args);
+    }
+}

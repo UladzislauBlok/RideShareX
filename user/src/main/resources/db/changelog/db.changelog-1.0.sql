facc32b40787dd7848edc4a55de4cca49c3bb8f1
Uladzislau Blok <blokv75@gmail.com>
2024-03-18 21:11:25 +0100

    fix(user-dev): Fix db.changelog-1.0.sql and docker-compose.yaml

--- user/src/main/resources/db/changelog/db.changelog-1.0.sql
+++ user/src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS users
 --changeset ublok:2
 CREATE TABLE IF NOT EXISTS documents (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id INTEGER REFERENCES users(id),
+    user_id uuid REFERENCES users(id),
     type VARCHAR(32) NOT NULL,
     number VARCHAR(64) NOT NULL UNIQUE ,
     creation_date DATE NOT NULL
@@ -28,10 +28,19 @@ CREATE TABLE IF NOT EXISTS documents (
 --changeset ublok:3
 CREATE TABLE IF NOT EXISTS cars (
     id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
-    user_id INTEGER REFERENCES users(id),
+    user_id uuid REFERENCES users(id),
     brand VARCHAR(32) NOT NULL,
     model VARCHAR(32) NOT NULL,
     year INTEGER,
     color VARCHAR(32),
     registration_number VARCHAR(32)
+);
+
+--changeset ublok:4
+CREATE TABLE IF NOT EXISTS ratings (
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
+    rated_user_id uuid REFERENCES users(id),
+    rating_user_id uuid REFERENCES users(id),
+    rating_value INT NOT NULL,
+    rated_at TIMESTAMP NOT NULL
 );

8e9a6c7041dd1dfba2e7c03ffe73c2855dc7f749
Uladzislau Blok <blokv75@gmail.com>
2024-03-18 19:54:11 +0100

    fix(user-dev): Change id type to uuid

--- user/src/main/resources/db/changelog/db.changelog-1.0.sql
+++ user/src/main/resources/db/changelog/db.changelog-1.0.sql
@@ -3,13 +3,14 @@
 --changeset ublok:1
 CREATE TABLE IF NOT EXISTS users
 (
-    id SERIAL PRIMARY KEY ,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     name VARCHAR(64) NOT NULL ,
     surname VARCHAR(64) NOT NULL ,
     email VARCHAR(128) NOT NULL UNIQUE ,
     password VARCHAR(256) NOT NULL ,
     registration_date DATE NOT NULL ,
     photo VARCHAR(128) ,
+    phone_number VARCHAR(32) ,
     animal_preference VARCHAR(32) ,
     conversation_preference VARCHAR(32) ,
     music_preference VARCHAR(32)
@@ -17,7 +18,7 @@ CREATE TABLE IF NOT EXISTS users
 
 --changeset ublok:2
 CREATE TABLE IF NOT EXISTS documents (
-    id SERIAL PRIMARY KEY,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id INTEGER REFERENCES users(id),
     type VARCHAR(32) NOT NULL,
     number VARCHAR(64) NOT NULL UNIQUE ,
@@ -26,7 +27,7 @@ CREATE TABLE IF NOT EXISTS documents (
 
 --changeset ublok:3
 CREATE TABLE IF NOT EXISTS cars (
-    id SERIAL PRIMARY KEY,
+    id uuid PRIMARY KEY DEFAULT gen_random_uuid(),
     user_id INTEGER REFERENCES users(id),
     brand VARCHAR(32) NOT NULL,
     model VARCHAR(32) NOT NULL,

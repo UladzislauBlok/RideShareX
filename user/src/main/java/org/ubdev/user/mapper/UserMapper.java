b5e8f5812f53e78bdc339bd207d3725ca2930b89
Uladzislau Blok <blokv75@gmail.com>
2024-03-20 19:30:43 +0100

    feat(user-dev): Complete UserMapper

--- user/src/main/java/org/ubdev/user/mapper/UserMapper.java
+++ user/src/main/java/org/ubdev/user/mapper/UserMapper.java
@@ -1,15 +1,15 @@
 package org.ubdev.user.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.ubdev.user.dto.CreateUserDto;
 import org.ubdev.user.dto.UserDto;
+import org.ubdev.user.dto.UserUpdateDto;
 import org.ubdev.user.model.User;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
     UserDto userToUserDto(User user);
-
-//    @Mapping(target = "id", ignore = true)
-//    User createNewUserWithoutId(CreateUserDto dto);
+    User createNewUserWithoutId(CreateUserDto dto);
+    void updateUserFromUserDto(UserUpdateDto userDto, @MappingTarget User user);
 }

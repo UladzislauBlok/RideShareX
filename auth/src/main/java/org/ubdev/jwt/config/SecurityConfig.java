ec6ea0050c986b7a6ac430d2160d5fda3b898ba4
Uladzislau Blok <blokv75@gmail.com>
2024-04-16 10:38:38 +0200

    feat(auth-dev): Add config and JwtConfigurer

---
+++ auth/src/main/java/org/ubdev/jwt/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package org.ubdev.jwt.config;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.RequestAttributeSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
+import org.ubdev.jwt.configurer.JwtAuthenticationConfigurer;
+import org.ubdev.jwt.factory.AccessTokenFactory;
+import org.ubdev.jwt.factory.RefreshTokenFactory;
+import org.ubdev.jwt.serializer.AccessTokenJwsStringSerializer;
+import org.ubdev.jwt.serializer.RefreshTokenJweStringSerializer;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public JwtAuthenticationConfigurer jwtAuthenticationConfigurer(ObjectMapper objectMapper,
+                                                                   JdbcTemplate jdbcTemplate,
+                                                                   @Value("${jwt.secret.jwe}") String secretStringJwe,
+                                                                   @Value("${jwt.secret.jws}") String secretStringJws) {
+        return new JwtAuthenticationConfigurer(securityContextRepository(),
+                new RefreshTokenFactory(),
+                new AccessTokenFactory(),
+                new RefreshTokenJweStringSerializer(secretStringJwe),
+                new AccessTokenJwsStringSerializer(secretStringJws),
+                objectMapper,
+                jdbcTemplate);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   JwtAuthenticationConfigurer jwtAuthenticationConfigurer) throws Exception {
+        http.with(jwtAuthenticationConfigurer, Customizer.withDefaults());
+
+        return http
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(sessionManager ->
+                        sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorizeHttpRequest ->
+                        authorizeHttpRequest.anyRequest().authenticated())
+                .build();
+    }
+
+    @Bean
+    public SecurityContextRepository securityContextRepository() {
+        return new RequestAttributeSecurityContextRepository();
+    }
+}
---
+++ auth/src/main/java/org/ubdev/jwt/configurer/JwtAuthenticationConfigurer.java
@@ -0,0 +1,53 @@
+package org.ubdev.jwt.configurer;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.access.ExceptionTranslationFilter;
+import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.ubdev.jwt.factory.TokenFactory;
+import org.ubdev.jwt.filter.RequestJwtTokensFilter;
+import org.ubdev.jwt.model.Token;
+import org.ubdev.jwt.serializer.JwtTokenStringSerializer;
+
+@Builder
+@RequiredArgsConstructor
+public class JwtAuthenticationConfigurer
+        extends AbstractHttpConfigurer<JwtAuthenticationConfigurer, HttpSecurity> {
+
+    private final SecurityContextRepository securityContextRepository;
+    private final TokenFactory<Authentication> refreshTokenFactory;
+    private final TokenFactory<Token> accessTokenFactory;
+    private final JwtTokenStringSerializer refreshTokenSerializer;
+    private final JwtTokenStringSerializer accessTokenSerializer;
+    private final ObjectMapper objectMapper;
+    private final JdbcTemplate jdbcTemplate;
+
+    @Override
+    public void init(HttpSecurity builder) throws Exception {
+        var csrfConfigurer = builder.getConfigurer(CsrfConfigurer.class);
+        if (csrfConfigurer != null) {
+            csrfConfigurer.ignoringRequestMatchers(new AntPathRequestMatcher("/jwt/tokens", "POST"));
+        }
+    }
+
+    @Override
+    public void configure(HttpSecurity builder) throws Exception {
+        var requestJwtTokensFilter = RequestJwtTokensFilter.builder()
+                .securityContextRepository(securityContextRepository)
+                .refreshTokenFactory(refreshTokenFactory)
+                .accessTokenFactory(accessTokenFactory)
+                .refreshTokenSerializer(refreshTokenSerializer)
+                .accessTokenSerializer(accessTokenSerializer)
+                .objectMapper(objectMapper)
+                .build();
+
+        builder.addFilterAfter(requestJwtTokensFilter, ExceptionTranslationFilter.class);
+    }
+}

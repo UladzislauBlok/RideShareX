ceca5022aa23aa782fdb07e2941d75183c1e7df7
Uladzislau Blok <blokv75@gmail.com>
2024-04-19 21:08:10 +0200

    feat(api-gateway-dev): Create JwtDeserializationFilter

---
+++ api-gateway/src/main/java/org/ubdev/security/path/PathConstants.java
@@ -0,0 +1,18 @@
+package org.ubdev.security.path;
+
+import lombok.experimental.UtilityClass;
+
+import java.util.Set;
+
+public class PathConstants {
+    public static final Set<String> PUBLIC_PATH = Set.of(
+            "/api/jwt/tokens"
+    );
+
+    public static final Set<String> REFRESH_TOKEN_PATH = Set.of(
+
+    );
+    private static final Set<String> ACCESS_TOKEN_PATH = Set.of(
+
+    );
+}

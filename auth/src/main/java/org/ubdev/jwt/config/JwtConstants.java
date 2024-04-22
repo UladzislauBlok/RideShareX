31b3ef3b56f29ef3ff1457194d3dfd285a01d210
Uladzislau Blok <blokv75@gmail.com>
2024-04-22 12:52:26 +0200

    feat(auth-dev): Update JwtConstants

--- auth/src/main/java/org/ubdev/jwt/config/JwtConstants.java
+++ auth/src/main/java/org/ubdev/jwt/config/JwtConstants.java
@@ -3,4 +3,9 @@ package org.ubdev.jwt.config;
 public class JwtConstants {
     public static final Long RefreshTokenTtlInDays = 1L;
     public static final Integer AccessTokenTtlInMinutes = 5;
+    public static final String AUTHORITIES = "authorities";
+    public static final String GRANT_PREFIX = "GRANT_";
+    public static final String JWT_REFRESH = "JWT_REFRESH";
+    public static final String JWT_LOGOUT = "JWT_LOGOUT";
+    public static final String ACCESS_DENIED_MESSAGE = "User must be authenticated";
 }

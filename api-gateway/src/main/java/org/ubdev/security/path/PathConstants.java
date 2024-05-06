9fe38b277c2d56a8f9cd3d7f3881e80f5bffa96f
Uladzislau Blok <blokv75@gmail.com>
2024-05-06 15:59:30 +0200

    fix(api-gateway-dev): Add http method checking for JwtDeserializationFilter

--- api-gateway/src/main/java/org/ubdev/security/path/PathConstants.java
+++ api-gateway/src/main/java/org/ubdev/security/path/PathConstants.java
@@ -1,15 +1,16 @@
 package org.ubdev.security.path;
 
-import lombok.experimental.UtilityClass;
-
 import java.util.Set;
 
 public class PathConstants {
     public static final Set<String> PUBLIC_PATH = Set.of(
-            "/api/jwt/tokens"
+            "POST|/api/v1/jwt/tokens",
+            "POST|/api/v1/users"
     );
 
     public static final Set<String> REFRESH_TOKEN_PATH = Set.of(
+            "POST|/api/v1/jwt/refresh",
+            "POST|/api/v1/jwt/logout"
 
     );
     private static final Set<String> ACCESS_TOKEN_PATH = Set.of(

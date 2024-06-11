3f550cc999ef5c5b9f5226b2909ed463793ac3de
Uladzislau Blok <blokv75@gmail.com>
2024-06-11 18:20:39 +0200

    feat(user-dev): create repository for current user documents

---
+++ user/src/main/java/org/ubdev/document/repository/DocumentByUserRepository.java
@@ -0,0 +1,11 @@
+package org.ubdev.document.repository;
+
+import org.ubdev.document.dto.DocumentDto;
+import org.ubdev.document.model.Document;
+
+import java.util.List;
+
+public interface DocumentByUserRepository {
+    DocumentDto saveDocumentWithUserEmail(Document document, String userEmail);
+    List<DocumentDto> getDocumentsByUserEmail(String userEmail);
+}
---
+++ user/src/main/java/org/ubdev/document/repository/DocumentByUserRepositoryImpl.java
@@ -0,0 +1,58 @@
+package org.ubdev.document.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.ubdev.document.dto.DocumentDto;
+import org.ubdev.document.model.Document;
+import org.ubdev.document.model.DocumentType;
+
+import java.util.List;
+import java.util.UUID;
+
+@RequiredArgsConstructor
+public class DocumentByUserRepositoryImpl implements DocumentByUserRepository {
+    private static final String SAVE_DOCUMENT_WITH_USER_ID = """
+            INSERT INTO documents(id, user_id, type, number, creation_date)
+            VALUES (:id, (SELECT id FROM users WHERE email = :email), :type, :number, :creationDate)
+            RETURNING id, user_id, type, number, creation_date
+            """;
+
+    private static final String FIND_ALL_BY_USER_EMAIL = """
+            SELECT id, user_id, type, number, creation_date FROM documents
+            where user_id = (SELECT id FROM users WHERE email = :email)
+            """;
+
+    private final NamedParameterJdbcTemplate jdbcTemplate;
+
+
+    @Override
+    public DocumentDto saveDocumentWithUserEmail(Document document, String userEmail) {
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("id", document.getId())
+                .addValue("email", userEmail)
+                .addValue("type", document.getType())
+                .addValue("number", document.getNumber())
+                .addValue("creationDate", document.getCreationDate());
+
+        return jdbcTemplate.queryForObject(SAVE_DOCUMENT_WITH_USER_ID, params, getDocumentDtoMapper());
+    }
+
+    @Override
+    public List<DocumentDto> getDocumentsByUserEmail(String userEmail) {
+        MapSqlParameterSource params = new MapSqlParameterSource()
+                .addValue("email", userEmail);
+
+        return jdbcTemplate.query(FIND_ALL_BY_USER_EMAIL, params, getDocumentDtoMapper());
+    }
+
+    private RowMapper<DocumentDto> getDocumentDtoMapper() {
+        return (rs, rowNum) -> new DocumentDto(
+                rs.getObject("id", UUID.class),
+                rs.getObject("user_id", UUID.class),
+                rs.getObject("type", DocumentType.class),
+                rs.getString("number"),
+                rs.getDate("creation_date").toLocalDate());
+    }
+}

7ea85ef914216ef073c6042181763f5a831f99d2
Uladzislau Blok <blokv75@gmail.com>
2024-06-11 18:22:00 +0200

    feat(user-dev): Complete DocumentController

---
+++ user/src/main/java/org/ubdev/document/controller/DocumentController.java
@@ -0,0 +1,53 @@
+package org.ubdev.document.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.ubdev.document.dto.DocumentCreateDto;
+import org.ubdev.document.dto.DocumentDto;
+import org.ubdev.document.service.DocumentService;
+
+import java.security.Principal;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/documents")
+public class DocumentController {
+    private final DocumentService documentService;
+
+    @GetMapping("/{id}")
+    public ResponseEntity<DocumentDto> getDocumentById(@PathVariable UUID id) {
+        return ResponseEntity.ok(documentService.getDocumentById(id));
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<List<DocumentDto>> getDocumentByUserId(@PathVariable UUID userId) {
+        return ResponseEntity.ok(documentService.getDocumentsByUserId(userId));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<DocumentDto>> getDocumentByUser(Principal principal) {
+        return ResponseEntity.ok(documentService.getDocumentsByUser(principal.getName()));
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<Page<DocumentDto>> getUsers(@RequestParam int page,
+                                                  @RequestParam int size) {
+        return ResponseEntity.ok(documentService.getAll(page, size));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteUserById(@PathVariable UUID id) {
+        documentService.deleteDocumentById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping
+    public ResponseEntity<?> createDocument(@RequestBody DocumentCreateDto dto, Principal principal) {
+        return ResponseEntity.ok(documentService.createDocument(dto, principal.getName()));
+    }
+}

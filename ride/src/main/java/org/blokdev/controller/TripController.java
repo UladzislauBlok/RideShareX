043a740d108e6f354fc8982b9383ef2c4d0957fd
Uladzislau Blok <blokv75@gmail.com>
2024-07-13 14:06:44 +0200

    feat(#1): Create TripController

---
+++ ride/src/main/java/org/blokdev/controller/TripController.java
@@ -0,0 +1,83 @@
+package org.blokdev.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.blokdev.dto.CreateTripDto;
+import org.blokdev.dto.JoinTripDecisionDto;
+import org.blokdev.dto.TripDto;
+import org.blokdev.service.TripService;
+import org.springframework.data.domain.Page;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/ride")
+@RequiredArgsConstructor
+public class TripController {
+    private final TripService tripService;
+
+    @PostMapping
+    public ResponseEntity<TripDto> createTrip(@RequestBody CreateTripDto dto, @RequestParam String ownerEmail) {
+        TripDto tripDto = tripService.createTrip(dto, ownerEmail);
+        return new ResponseEntity<>(tripDto, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<TripDto>> getTripsByRouteAndTime(@RequestParam String departureCity,
+                                                                @RequestParam String arrivalCity,
+                                                                @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime departureTime,
+                                                                @RequestParam int pageNum,
+                                                                @RequestParam int pageSize) {
+        Page<TripDto> trips = tripService.getTripsByRouteAndTime(departureCity, arrivalCity, departureTime, pageNum, pageSize);
+        return new ResponseEntity<>(trips, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTrip(@PathVariable UUID id) {
+        tripService.deleteTrip(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    @PostMapping("/{tripId}/join-requests")
+    public ResponseEntity<Void> requestJoinTrip(@PathVariable UUID tripId, @RequestParam String userEmail) {
+        tripService.requestJoinTrip(tripId, userEmail);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/join-requests/decision")
+    public ResponseEntity<Void> processJoinTripDecision(@RequestBody JoinTripDecisionDto dto, @RequestParam String currentUserEmail) {
+        tripService.processJoinTripDecision(dto, currentUserEmail);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/{tripId}/start")
+    public ResponseEntity<Void> startTrip(@PathVariable UUID tripId, @RequestParam String currentUserEmail) {
+        tripService.startTrip(tripId, currentUserEmail);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/{tripId}/end")
+    public ResponseEntity<Void> endTrip(@PathVariable UUID tripId, @RequestParam String currentUserEmail) {
+        tripService.endTrip(tripId, currentUserEmail);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/ratings")
+    public ResponseEntity<Void> createRating(@RequestParam UUID ratedUserId,
+                                             @RequestParam Integer ratingValue,
+                                             @RequestParam String currentUserEmail) {
+        tripService.createRating(ratedUserId, ratingValue, currentUserEmail);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+}
